54b9a3c0f20e0d32d48c70decaec26c585eaf20f
BetaPeche <lolopellegrino@gmail.com>
2024-07-03 10:00:41 +0200

    Show user name and image if he is connected

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+'use client'
 import Link from 'next/link'
 import Logo from './Logo'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 const Header = () => {
+    const { data: session } = useSession()
     return (
         <header className="flex w-full max-w-[1140px] items-center justify-between py-6">
             <Logo />
@@ -19,9 +23,30 @@ const Header = () => {
                     Company
                 </Link>
             </div>
-            <button className="rounded-[10px] bg-[#E9E3FF] px-4 py-3 text-sm font-bold text-primary hover:bg-purple-100">
-                Sign Up
-            </button>
+            {!session ? (
+                <Link
+                    href="/signin"
+                    className="rounded-[10px] bg-[#E9E3FF] px-4 py-3 text-sm font-bold text-primary hover:bg-purple-100"
+                >
+                    Sign In
+                </Link>
+            ) : (
+                <div className="flex items-center gap-2">
+                    <Image
+                        src={session.user?.image as string}
+                        width={40}
+                        height={40}
+                        alt="avatar"
+                        className="rounded-full"
+                    />
+                    <Link
+                        href="/signin"
+                        className="text-sm font-bold text-primary"
+                    >
+                        {session.user?.name}
+                    </Link>
+                </div>
+            )}
         </header>
     )
 }
