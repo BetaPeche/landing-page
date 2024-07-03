493e86c14a2efe268f296df0c0b9bb7e0d2d9252
BetaPeche <lolopellegrino@gmail.com>
2024-07-03 09:59:12 +0200

    Add SessionWrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
+import SessionWrapper from '@/components/SessionWrapper'
 
 const plusJakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
@@ -15,12 +16,14 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="fr" className="">
-            <body
-                className={`${plusJakarta.className} flex flex-col items-center bg-slate-100 p-5 dark:bg-slate-900`}
-            >
-                {children}
-            </body>
-        </html>
+        <SessionWrapper>
+            <html lang="fr" className="">
+                <body
+                    className={`${plusJakarta.className} flex flex-col items-center bg-slate-100 p-5 dark:bg-slate-900`}
+                >
+                    {children}
+                </body>
+            </html>
+        </SessionWrapper>
     )
 }
