aa9586f77ace9b4ae5eb7630027fa1e7b0c4ff6d
BetaPeche <lolopellegrino@gmail.com>
2024-08-20 11:41:38 +0200

    Fix animation

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,12 +2,10 @@ const Loader = () => {
     return (
         <>
             <div
-                className="text-surface absolute left-[50%] top-[50%] inline-block h-8 w-8 translate-x-[-50%] translate-y-[-50%] animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                className="text-surface absolute left-[calc(50%-1rem)] top-[calc(50%-1rem)] inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status"
             >
-                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                    Loading...
-                </span>
+                <span className="sr-only">Loading...</span>
             </div>
         </>
     )
