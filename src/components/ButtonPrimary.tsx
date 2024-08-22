d6ccb0e81a47c2b63099986b98df3222caddf86c
BetaPeche <lolopellegrino@gmail.com>
2024-08-22 16:27:45 +0200

    Create component

---
+++ src/components/ButtonPrimary.tsx
@@ -0,0 +1,9 @@
+const ButtonPrimary = () => {
+    return (
+        <button className="max-w-fit rounded-[10px] bg-primary px-7 py-4 text-sm font-bold text-slate-50 hover:bg-indigo-600">
+            Get started now
+        </button>
+    )
+}
+
+export default ButtonPrimary
